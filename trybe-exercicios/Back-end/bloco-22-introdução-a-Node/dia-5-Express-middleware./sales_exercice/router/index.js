cbc1ad64a8f00c59b37b3c6a6e0d0e13bcffdfdf
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-05-15 18:20:21 -0300

    feat: colcoando validações na rota.

--- trybe-exercicios/Back-end/bloco-22-introdução-a-Node/dia-5-Express-middleware./sales_exercice/router/index.js
+++ trybe-exercicios/Back-end/bloco-22-introdução-a-Node/dia-5-Express-middleware./sales_exercice/router/index.js
@@ -5,7 +5,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 //importando a validação do productName
-const validateProductName = require('../middleware/validation');
+const validateProductName = require('../middlewares/validationProductName');
+
+//importando validação de infos:
+const validateInfos = require('../middlewares/validationInfos');
 
 //instanciando o express:
 const app = express();
@@ -14,7 +17,7 @@ app.use(bodyParser.json());
 
 // criando a porta: POST http://localhost:3001/sales,
 // necessário criar meddleware de validação na pasta middleware!
-app.post('/sales', validateProductName ,(req, res) => (
+app.post('/sales', validateProductName, validateInfos, (req, res) => (
     res.status(201).json({ message: 'sale criado com sucesso'})
 ));
 
