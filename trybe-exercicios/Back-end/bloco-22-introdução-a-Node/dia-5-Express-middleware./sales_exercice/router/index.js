a557bce3fc69ae2834f3c794724e700895335749
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-05-15 17:57:53 -0300

    feat: criando middleware de productName.

--- trybe-exercicios/Back-end/bloco-22-introdução-a-Node/dia-5-Express-middleware./sales_exercice/router/index.js
+++ trybe-exercicios/Back-end/bloco-22-introdução-a-Node/dia-5-Express-middleware./sales_exercice/router/index.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser');
 //instanciando o express:
 const app = express();
 
-// criando a porta:
-// necessário criar meddleware de validação
+// criando a porta: POST http://localhost:3001/sales,
+// necessário criar meddleware de validação na pasta middleware!
 app.post('/sales', (req, res) => (
     res.status(201).json({ message: 'sale criado com sucesso'})
 ));
