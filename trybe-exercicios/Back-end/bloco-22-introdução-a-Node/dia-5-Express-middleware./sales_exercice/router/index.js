8d57cd527255fe5681d42e905dd2dcfce0dc3b70
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-05-16 10:55:36 -0300

    feat: importando crypto e validações.

--- trybe-exercicios/Back-end/bloco-22-introdução-a-Node/dia-5-Express-middleware./sales_exercice/router/index.js
+++ trybe-exercicios/Back-end/bloco-22-introdução-a-Node/dia-5-Express-middleware./sales_exercice/router/index.js
@@ -1,14 +1,21 @@
 //  Configurações iniciais do servidor:
 
-// importando o express e o body-parser:
+// importando o express, o body-parser e o crypto:
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 //importando a validação do productName
 const validateProductName = require('../middlewares/validationProductName');
 
 //importando validação de infos:
 const validateInfos = require('../middlewares/validationInfos');
+
+//importando validação de saledate
+const validateSaleDate = require('../middlewares/validationSaleDate');
+
+//importando validação de warrantyPeriod 
+const validateWarrantyPeriod = require('../middlewares/validationWarrantyPeriod');
 
 //instanciando o express:
 const app = express();
@@ -17,7 +24,8 @@ app.use(bodyParser.json());
 
 // criando a porta: POST http://localhost:3001/sales,
 // necessário criar meddleware de validação na pasta middleware!
-app.post('/sales', validateProductName, validateInfos, (req, res) => (
+// a ordem dos middleware importam, é necessario validar primeiro se o objeto existe e só depois o seu conteúdo.
+app.post('/sales', validateProductName, validateInfos, validateSaleDate, (req, res) => (
     res.status(201).json({ message: 'sale criado com sucesso'})
 ));
 
