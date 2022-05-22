a767c169212a23c798089aa3b20d32ddd31f909a
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-05-22 17:56:32 -0300

    :sparkles:feat: criando a conexão.

--- trybe-exercicios/Back-end/bloco-23-Nodejs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-Arquitetura-de-Software-Camada-de-Model/db/connection.js
+++ trybe-exercicios/Back-end/bloco-23-Nodejs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-Arquitetura-de-Software-Camada-de-Model/db/connection.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    
+    // host: 'localhost',
+	// user: 'seu_usuario',
+	// password: 'sua_senha',
+	// database: 'users_crud',
 })
+module.exports = connection;
