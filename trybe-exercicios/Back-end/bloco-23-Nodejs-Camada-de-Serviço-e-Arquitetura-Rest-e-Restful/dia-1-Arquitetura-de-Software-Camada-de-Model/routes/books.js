26f5bf652211692942a51533778f36b465511084
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-05-22 19:17:26 -0300

    :recycle:	refactor: Refatorando as rotas pra MSC.

--- trybe-exercicios/Back-end/bloco-23-Nodejs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-Arquitetura-de-Software-Camada-de-Model/routes/books.js
+++ trybe-exercicios/Back-end/bloco-23-Nodejs-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-1-Arquitetura-de-Software-Camada-de-Model/routes/books.js
@@ -1,36 +1,15 @@
 const express = require('express');
+const rescue = require('express-rescue');
+
+const Book = require('./controllers/Book');
+const errorMiddleware = require('./middlewares/error');
+
 const router = express.Router();
 
-const book = require('../models/BooksModel')
+const Book = require('../models/Book')
 
-router.get('/books', async (req, res) => {
-    const { author_id } = req.query;
-  
-    const books = (author_id)
-    ? await book.getByAuthorId(author_id)
-    : await book.getAll();
-  
-    res.status(200).json(books);
-  });
+router.get('/books', rescue(Book.getAll));
+router.get('/books/:id', rescue(Book.findById));
+router.post('/books', rescue(Book.createBook));
 
-router.get('/book/:id', async (req, res) => {
-    const { id } = req.params;
-  
-    const book = await Book.getById(id);
-  
-    if (!book) return res.status(404).json({ message: 'Book not found' });
-  
-    res.status(200).json(book);
-  });
-
-router.post('/books', async (req, res) => {
-    const { title, author_id } = req.body;
-    
-    if (!await book.validationTitleAndId(title, author_id)) {
-      return res.status(400).json({ message: 'Dados inválidos' });
-    }
-    
-    await book.createNewBook(title, author_id);
-    
-    res.status(201).json({ message: 'Livro criado com sucesso! '});
-    });
+router.use(errorMiddleware);
